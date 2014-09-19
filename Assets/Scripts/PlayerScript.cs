a263f5beba3c9847a2097480d09bb8a208999fc0
aash29 <aash29@gmail.com>
2014-09-19 09:02:03 +0400

    textures

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,10 +7,19 @@ public class PlayerScript : MonoBehaviour
 	/// <summary>
 	/// 1 - The speed of the ship
 	/// </summary>
-	public Vector2 speed = new Vector2(50, 50);
-	
+	public Vector3 speed = new Vector3(50, 50, 0);
+
+	public float linearDamping = 0.5f;
+
+	public float angularDamping = 0.5f;
+
+	private Vector3 screenPoint;
+	private Vector3 offset;
+
 	// 2 - Store the movement
-	private Vector2 movement;
+	private Vector3 movement;
+
+	static int selectedCoin = 0;
 	
 	void Update()
 	{
@@ -19,15 +28,42 @@ public class PlayerScript : MonoBehaviour
 		float inputY = Input.GetAxis("Vertical");
 		
 		// 4 - Movement per direction
-		movement = new Vector2(
+		movement = new Vector3(
 			speed.x * inputX,
-			speed.y * inputY);
-		
+			speed.y * inputY,
+			0.0f);
 	}
 	
 	void FixedUpdate()
 	{
 		// 5 - Move the game object
-		rigidbody2D.velocity = movement;
+		rigidbody.velocity = movement;
+		rigidbody.drag = linearDamping;
+		rigidbody.angularDrag = angularDamping;
+	}
+
+	void OnDrawGizmos() {
+		Vector3 center = renderer.bounds.center;
+		float radius = renderer.bounds.extents.magnitude;
+		Gizmos.color = Color.white;
+		Gizmos.DrawWireSphere(center, radius);
+	}
+
+	void OnMouseDown()
+	{
+		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(
+			new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}
+	
+	
+	void OnMouseDrag()
+	{
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+		transform.position = curPosition;
 	}
 }
+
